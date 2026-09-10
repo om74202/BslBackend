c5076bfff7a4da5abc7fa62c23c5ef1f9b8a1a7e
om74202 <ommishra74202@gmail.com>
2026-09-10 13:32:11 +0530

    init

--- functions/mqtt.js
+++ functions/mqtt.js
@@ -1,7 +1,7 @@
 // utils/mqttPublisher.js
 const mqtt = require("mqtt");
 
-const MQTT_URL = "mqtt://20.197.46.212:3006";
+const MQTT_URL = "mqtt://20.197.30.129:3006";
 const MQTT_USERNAME = "opsight";
 const MQTT_PASSWORD = "Devops@opsight1";
 
