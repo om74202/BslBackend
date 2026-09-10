a2509be56c242a5f2edaf336dff5a884dc9af6ce
om74202 <ommishra74202@gmail.com>
2026-09-10 13:53:59 +0530

    init

--- db/influxDB/influx.js
+++ db/influxDB/influx.js
@@ -7,8 +7,8 @@ const { env } = require('process');
 
 // Set up environment variables or directly replace with your values
 
-const url ="http://20.197.46.212:8086"
-const token = "QC92_DI9dW7uKpORhzaEX7xIi3MmAd20hAALxeOs0Gy9_JgozFPqjUTVMXJjHkTyl1m2wzfIbUdR5K4F7N8TVA=="
+const url ="http://20.197.30.129:3005"
+const token ="ZrszlOgk6M9zMuoMuM6JF8FWGvhlrBrOZfgv8uHMnMuG2Sp4NUiw6qwtSg217L71McsJ5QCPooXrMJ5Io2rjuw==";
 //8tecUTFpztrGUmSwoFZ_1g-7TTXp_MitnVf9lhna4IxeDWDPFnQV41pyJ-ZCISFk8Ehc89kuWXBGW0xzuMq16A==
 // Initialize the InfluxDB client
 const influxDB = new InfluxDB({
